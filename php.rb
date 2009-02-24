cff6e8fcc5b2f3266daf1da5d9364a1649423626
Jason Stewart <jstewart@fusionary.com>
2009-02-24 11:29:41 -0500

    Preliminary PHP support.

---
+++ php.rb
@@ -0,0 +1,27 @@
+Capistrano::Configuration.instance.load do
+  require File.dirname(__FILE__) + '/util'
+
+  # Default values
+  set :keep_releases,   3
+  set :app_symlinks,    nil
+
+  # Callbacks
+  after "deploy",           "deploy.cleanup"
+  after "deploy",           "util:notify"
+  after "deploy:symlink",   "fusionary:symlink_extras"
+
+  namespace :deploy do
+    task :restart do
+      puts "This is a no-op in PHP"
+    end
+  end
+
+  namespace :fusionary do
+    desc "Setup additional symlinks to shared directories"
+    task :symlink_extras, :roles => [:app, :web] do
+      if app_symlinks
+        app_symlinks.each { |link| run "ln -nfs #{shared_path}/#{link} #{current_path}/#{link}" }
+      end
+    end
+  end
+end

fe96f5b57f3583787582aa7fc6a7e16edecb460d
Jason Stewart <jstewart@fusionary.com>
2009-03-25 13:57:06 -0400

    Make php and rails database sync use the same codebase.

---
+++ db.rb
@@ -0,0 +1,102 @@
+Capistrano::Configuration.instance.load do
+
+
+  namespace :db do
+    desc "Copy database from server to local machine"
+    task :sync_to_local, :roles => [:db] do
+      set_config_vars
+      continue = Capistrano::CLI.ui.ask "This task will overwrite your existing #{rails_env} data. Proceed? (y/N)"
+      if continue =~ /[Yy]/
+        mysql_dump    = mysqldump(remote_dump_path,  remote_dbconfig['database'],
+                                  :u => remote_dbconfig['username'], :p => remote_dbconfig['password'],
+                                  :h => remote_dbconfig['host'], :compress => true)
+
+        mysql_import  = mysqlimport(local_path, local_dbconfig['database'], :mysql_cmd => mysql_cmd,
+                                    :u => local_dbconfig['username'], :p => local_dbconfig['password'],
+                                    :h => local_dbconfig['host'], :compress => true)
+
+        run mysql_dump do |ch, _, out| 
+          puts out 
+        end
+
+        download remote_dump_path, local_path
+
+        run "rm #{remote_dump_path}"
+
+        puts "Running local mysql import from #{rails_env} data..." 
+        `#{mysql_import}`
+        `rm #{uncompressed_path(local_path)}` 
+      end
+    end
+
+    desc "Copy local database to server" 
+    task :sync_to_remote, :roles => [:db] do  
+      set_config_vars
+      continue = Capistrano::CLI.ui.ask "CAUTION!!!! This task will overwrite your existing #{rails_env} data REMOTELY. Proceed? (y/N)"
+      if continue =~ /[Yy]/
+        mysql_dump    = mysqldump(local_path,  local_dbconfig['database'],
+                                  :u => local_dbconfig['username'], :p => local_dbconfig['password'],
+                                  :h => local_dbconfig['host'], :compress => true, :mysqldump_cmd => "mysqldump5")
+
+        mysql_import  = mysqlimport(remote_dump_path, remote_dbconfig['database'], :mysql_cmd => "mysql",
+                                    :u => remote_dbconfig['username'], :p => remote_dbconfig['password'],
+                                    :h => remote_dbconfig['host'], :compress => true)
+
+        `#{mysql_dump}`
+        upload local_path, remote_dump_path
+
+        puts "Running remote mysql import from #{rails_env} data..." 
+        run mysql_import
+        run "rm #{uncompressed_path(remote_dump_path)}"
+      end
+
+
+    end
+
+    def mysqldump(dumpfile, database, options={})
+      cmd         = options.delete(:mysqldump_cmd) || "mysqldump"
+      compress  =   options.delete(:compress)
+      opts      =   create_option_string(options)
+      dump_cmd  =   "#{cmd} #{opts} #{database}"
+      dump_cmd +=   " | gzip -f"  if compress
+      dump_cmd +=   " > #{dumpfile}"
+    end
+
+    def mysqlimport(dumpfile, database, options={})
+      compress    = options.delete(:compress)
+      cmd         = options.delete(:mysql_cmd) || "mysql"
+      if compress
+        import_cmd = "gunzip -f #{dumpfile} && "
+        dumpfile = File.dirname(dumpfile) + "/" + File.basename(dumpfile, ".gz") 
+      else
+        import_cmd = ""
+      end
+
+      import_cmd  += "#{cmd} #{create_option_string(options)} #{database} < #{dumpfile}"
+    end
+
+    def create_option_string(options)
+      options.inject([]) do |ary, (k, v)|
+        # Due to lame mysql command like requirement
+        ary << (k == :p ? "-#{k}#{v}" : "-#{k} #{v}")
+      end.join(" ")
+    end
+
+    def uncompressed_path(orig_path)
+      File.dirname(orig_path) + "/" + File.basename(orig_path, ".gz")
+    end
+
+    def set_config_vars
+      set :database_yml_path,   fetch(:database_yml_path, "config/database.yml")
+      set :mysql_cmd,           "mysql"
+
+      database_yml = ""
+      run "cat #{shared_path}/config/database.yml" do |_, _, database_yml| end
+      set :rails_env,         'development' 
+      set :remote_dbconfig,   YAML::load(database_yml)[rails_env]
+      set :local_dbconfig,    YAML::load(File.open(database_yml_path))[rails_env]
+      set :remote_dump_path,  "#{current_path}/tmp/#{rails_env}_dump.sql.gz"
+      set :local_path,        'tmp/' + File.basename(remote_dump_path)
+    end
+  end 
+end
--- rails.rb
+++ rails.rb
@@ -1,6 +1,7 @@
 Capistrano::Configuration.instance.load do
   require 'yaml'
   require File.dirname(__FILE__) + '/util'
+  require File.dirname(__FILE__) + '/db'
   require File.dirname(__FILE__) + '/ferret' if (File.exist?('script/ferret_server') || File.exist?('script/ferret_start'))
 
   #TODO: Remove this when we migrate all of our apps to passenger
@@ -51,108 +52,4 @@ Capistrano::Configuration.instance.load do
       end
     end
   end
-
-  namespace :db do
-
-    
-
-    desc "Copy database from server to local machine"
-    task :sync_to_local, :roles => [:db] do
-      continue = Capistrano::CLI.ui.ask "This task will overwrite your existing #{rails_env} data. Proceed? (y/N)"
-      if continue =~ /[Yy]/
-
-        set_config_vars
-
-        mysql_dump    = mysqldump(remote_path,  remote_dbconfig['database'],
-                                  :u => remote_dbconfig['username'], :p => remote_dbconfig['password'],
-                                  :h => remote_dbconfig['host'], :compress => true)
-
-        mysql_import  = mysqlimport(local_path, local_dbconfig['database'], :mysql_cmd => mysql_cmd,
-                                  :u => local_dbconfig['username'], :p => local_dbconfig['password'],
-                                  :h => local_dbconfig['host'], :compress => true)
-
-        run mysql_dump do |ch, _, out| 
-          puts out 
-        end
-
-        download remote_path, local_path
-
-        run "rm #{remote_path}"
-
-        puts "Running local mysql import from #{rails_env} data..." 
-        `#{mysql_import}`
-        `rm #{uncompressed_path(local_path)}` 
-      end
-    end
-
-    desc "Copy local database to server" 
-    task :sync_to_remote, :roles => [:db] do 
-      continue = Capistrano::CLI.ui.ask "CAUTION!!!! This task will overwrite your existing #{rails_env} data REMOTELY. Proceed? (y/N)"
-      if continue =~ /[Yy]/
-        set_config_vars
-
-        mysql_dump    = mysqldump(local_path,  local_dbconfig['database'],
-                                  :u => local_dbconfig['username'], :p => local_dbconfig['password'],
-                                  :h => local_dbconfig['host'], :compress => true, :mysqldump_cmd => "mysqldump5")
-
-        mysql_import  = mysqlimport(remote_path, remote_dbconfig['database'], :mysql_cmd => "mysql",
-                                  :u => remote_dbconfig['username'], :p => remote_dbconfig['password'],
-                                  :h => remote_dbconfig['host'], :compress => true)
-
-        `#{mysql_dump}`
-        upload local_path, remote_path
-
-        puts "Running remote mysql import from #{rails_env} data..." 
-        run mysql_import
-        run "rm #{uncompressed_path(remote_path)}"
-      end
-
-
-    end
-
-    def set_config_vars
-        fetch(:mysql_cmd, "mysql")
-
-        database_yml  = ""
-        run "cat #{shared_path}/config/database.yml" do |_, _, database_yml| end
-        set :remote_dbconfig,   YAML::load(database_yml)[rails_env]
-        set :local_dbconfig,    YAML::load(File.open("config/database.yml"))[rails_env]
-        set :remote_path,       "#{current_path}/tmp/#{rails_env}_dump.sql.gz"
-        set :local_path,        'tmp/' + File.basename(remote_path)
-    end
-
-    def mysqldump(dumpfile, database, options={})
-      cmd         = options.delete(:mysqldump_cmd) || "mysqldump"
-      compress  =   options.delete(:compress)
-      opts      =   create_option_string(options)
-      dump_cmd  =   "#{cmd} #{opts} #{database}"
-      dump_cmd +=   " | gzip -f"  if compress
-      dump_cmd +=   " > #{dumpfile}"
-    end
-
-    def mysqlimport(dumpfile, database, options={})
-      compress    = options.delete(:compress)
-      cmd         = options.delete(:mysql_cmd) || "mysql"
-      if compress
-        import_cmd = "gunzip -f #{dumpfile} && "
-        dumpfile = File.dirname(dumpfile) + "/" + File.basename(dumpfile, ".gz") 
-      else
-        import_cmd = ""
-      end
-
-      import_cmd  += "#{cmd} #{create_option_string(options)} #{database} < #{dumpfile}"
-    end
-
-    def create_option_string(options)
-      options.inject([]) do |ary, (k, v)|
-        # Due to lame mysql command like requirement
-        ary << (k == :p ? "-#{k}#{v}" : "-#{k} #{v}")
-      end.join(" ")
-    end
-
-    def uncompressed_path(orig_path)
-      File.dirname(orig_path) + "/" + File.basename(orig_path, ".gz")
-    end
-
-  end 
 end

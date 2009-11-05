40f6b00553ba883d9de344fb90b8788d3b876849
Jason Stewart <jstewart@fusionary.com>
2009-11-05 17:22:51 -0500

    Use global default config (/etc/oauth_yammer.yml) for notifier if local does not exist.

--- lib/notifier.rb
+++ lib/notifier.rb
@@ -6,8 +6,10 @@ require 'yammer4r/yammer4r'
 
 class Notifier
   def self.say(msg)
-    config_path = File.dirname(__FILE__) + '/../config/oauth.yml'
-    y = Yammer::Client.new(:config => config_path) 
+    default_config  = "/etc/oauth_yammer.yml"
+    local_config    = File.dirname(__FILE__) + '/../config/oauth.yml'
+    config_path     = File.exist?(local_config) ? local_config : default_config
+    y = Yammer::Client.new(:config => config_path)
     y.message(:post, :body => msg)
   end
 end
